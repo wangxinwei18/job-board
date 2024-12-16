bcd9e162ddffeac9530457f0b8870a53f0175e0d
wangxinwei18 <wangxinwei18@hotmail.com>
2024-12-16 20:27:32 +0800

    version1 push jobs

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import JobList from "../components/JobList";
+import { useJobs } from "../lib/graphql/hooks";
+import PaginationBar from "../components/PaginationBar";
+
+const JOBS_PER_PAGE = 5;
+function HomePage() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { jobs, loading, error } = useJobs(
+    JOBS_PER_PAGE,
+    (currentPage - 1) * JOBS_PER_PAGE
+  );
+
+  console.log("[HomePage", { jobs, loading, error });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div className="has-text-danger">Data unavailable</div>;
+  }
+  const totalPages = Math.ceil(jobs.totalCount / JOBS_PER_PAGE);
+  return (
+    <div>
+      <h1 className="title">Job Board</h1>
+      <PaginationBar
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+      <JobList jobs={jobs.items} />
+    </div>
+  );
+}
+
+export default HomePage;
